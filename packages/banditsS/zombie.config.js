32a4a782f249d3f9273a181340795213494981b6
SergGpt <126785643+SergGpt@users.noreply.github.com>
2026-03-15 21:12:04 +0500

    Настроить шанс выпадения сумки зомби на 25%

--- packages/banditsS/zombie.config.js
+++ packages/banditsS/zombie.config.js
@@ -35,6 +35,7 @@ const ZOMBIE_CONFIG = {
 
     loot: {
         bagModel: 'prop_cs_heist_bag_01',
+        bagDropChancePercent: 25,
         interactDistance: 2.2,
         cancelDistance: 3.5,
         itemIds: [234, 235, 237, 238, 239, 240, 241, 242, 243, 244],
