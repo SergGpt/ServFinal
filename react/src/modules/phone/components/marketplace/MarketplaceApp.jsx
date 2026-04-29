a902ce0bb1896bba13335fe347e9412ecc4dc18e
SergGpt <126785643+SergGpt@users.noreply.github.com>
2026-04-29 10:01:22 +0500

    Auto-open fullscreen marketplace when legacy phone app route is triggered

--- react/src/modules/phone/components/marketplace/MarketplaceApp.jsx
+++ react/src/modules/phone/components/marketplace/MarketplaceApp.jsx
@@ -1,40 +1,17 @@
 /* eslint-disable no-undef */
-import React from 'react';
+import React, { useEffect } from 'react';
 import { closeApp } from '../../actions/apps.actions';
 import { connect } from 'react-redux';
 
-const btnStyle = {
-    border: 'none',
-    borderRadius: 8,
-    padding: '10px 12px',
-    cursor: 'pointer'
-};
-
 const MarketplaceApp = ({ closeAppAction }) => {
-    const onOpenFullscreen = () => {
+    useEffect(() => {
         if (typeof mp !== 'undefined' && mp.trigger) {
             mp.trigger('callRemote', 'marketplace.phone.open.fullscreen');
         }
         closeAppAction();
-    };
+    }, [closeAppAction]);
 
-    return (
-        <div style={{ padding: 12, color: '#fff' }}>
-            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: 10 }}>
-                <strong>Маркетплейс</strong>
-                <button style={btnStyle} onClick={closeAppAction}>Назад</button>
-            </div>
-
-            <div style={{ background: 'rgba(0,0,0,.35)', borderRadius: 10, padding: 12 }}>
-                <div style={{ marginBottom: 10, opacity: 0.9 }}>
-                    Полноценный маркетплейс открывается отдельным большим окном.
-                </div>
-                <button style={{ ...btnStyle, width: '100%' }} onClick={onOpenFullscreen}>
-                    Открыть маркетплейс
-                </button>
-            </div>
-        </div>
-    );
+    return null;
 };
 
 const mapDispatchToProps = (dispatch) => ({
